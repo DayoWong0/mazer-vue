98e62bdf2bc4125a9b8cccd699d6fbde10aa4886
DayoWong <zz15698>
2021-02-10 23:54:35 +0800

    rest component finished,known bug: 1. sidebar do not auto toggle when select a new page 2. sidebar scroll problem on mobile

--- src/plugins/bootstrap.js
+++ src/plugins/bootstrap.js
@@ -1,3 +1,4 @@
-import 'bootstrap'
-// 因为需要自定义 css 不引入 bootstrap 的 css
-// import 'bootstrap/scss/bootstrap.scss'
+// Include bootstrap JS only
+if (process.browser) {
+    window.bootstrap = require('bootstrap')
+}
--- src/router/index.js
+++ src/router/index.js
@@ -21,23 +21,112 @@ export default new VueRouter({
                 }
             ]
         },
-        // {
-        //     path: '/',
-        //     component: User,
-        //     children: [
-        //         {
-        //             // 当 /user/:id/profile 匹配成功，
-        //             // UserProfile 会被渲染在 User 的 <router-view> 中
-        //             path: 'profile',
-        //             component: UserProfile
-        //         },
-        //         {
-        //             // 当 /user/:id/posts 匹配成功
-        //             // UserPosts 会被渲染在 User 的 <router-view> 中
-        //             path: 'posts',
-        //             component: UserPosts
-        //         }
-        //     ]
-        // }
+        {
+            path: '/components',
+            component: defaultLayout,
+            children: [
+                {
+                    path: 'alert',
+                    component: () => import('@/pages/components/alert'),
+                },
+                {
+                    path: 'badge',
+                    component: () => import('@/pages/components/badge'),
+                },
+                {
+                    path: 'breadcrumb',
+                    component: () => import('@/pages/components/breadcrumb'),
+                },
+                {
+                    path: 'button',
+                    component: () => import('@/pages/components/button'),
+                },
+                {
+                    path: 'card',
+                    component: () => import('@/pages/components/card'),
+                },
+                {
+                    path: 'carousel',
+                    component: () => import('@/pages/components/carousel'),
+                },
+                {
+                    path: 'dropdown',
+                    component: () => import('@/pages/components/dropdown'),
+                },
+                {
+                    path: 'list-group',
+                    component: () => import('@/pages/components/list-group'),
+                },
+                {
+                    path: 'modal',
+                    component: () => import('@/pages/components/modal'),
+                },
+                {
+                    path: 'navs',
+                    component: () => import('@/pages/components/navs'),
+                },
+                {
+                    path: 'pagination',
+                    component: () => import('@/pages/components/pagination'),
+                },
+                {
+                    path: 'progress',
+                    component: () => import('@/pages/components/progress'),
+                },
+                {
+                    path: 'spinner',
+                    component: () => import('@/pages/components/spinner'),
+                },
+                {
+                    path: 'tooltip',
+                    component: () => import('@/pages/components/tooltip'),
+                },
+
+            ]
+        },
+        {
+            path: '/extra-components',
+            component: defaultLayout,
+            children: [
+                {
+                    path: 'avatar',
+                    component: () => import('@/pages/extra-components/avatar'),
+                },
+                {
+                    path: 'sweetalert',
+                    component: () => import('@/pages/extra-components/sweetalert'),
+                },
+                {
+                    path: 'toastify',
+                    component: () => import('@/pages/extra-components/toastify'),
+                },
+                {
+                    path: 'rating',
+                    component: () => import('@/pages/extra-components/rating'),
+                },
+                {
+                    path: 'divider',
+                    component: () => import('@/pages/extra-components/divider'),
+                }
+            ]
+        },
+        {
+            // this layout need change
+            path: '/layouts',
+            component: defaultLayout,
+            children: [
+                {
+                    path: 'default',
+                    component: () => import('@/pages/layouts/default'),
+                },
+                {
+                    path: '1-column',
+                    component: () => import('@/pages/layouts/1-column'),
+                },
+                {
+                    path: 'vertical',
+                    component: () => import('@/pages/layouts/vertical'),
+                },
+            ]}
     ]
 });
